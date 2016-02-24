86f40e170fdb6b9807421b9208348717d7571743
Courtland Caldwell <courtland@mattermark.com>
2016-02-24 00:26:10 +0000

    Only configure credentials if they are overridden

--- lib/better_sqs/configuration.rb
+++ lib/better_sqs/configuration.rb
@@ -11,14 +11,21 @@ module BetterSqs
       @sqs_message_deferral_seconds = 60
       @aws_access_key_id      = ENV["AWS_ACCESS_KEY_ID"]
       @aws_secret_access_key  = ENV["AWS_SECRET_ACCESS_KEY"]
-      @region                 = ENV["AWS_REGION"]
+      @region                 = ENV["AWS_REGION"] || "us-east-1"
     end
 
     def configure_aws
+      return configure_region_only unless aws_secret_access_key && aws_access_key_id
       Aws.config.update(
         region:      region,
         credentials: Aws::Credentials.new(aws_access_key_id, aws_secret_access_key),
       )
+    end
+
+    private
+
+    def configure_region_only
+      Aws.config.update(region: region)
     end
   end
 end
